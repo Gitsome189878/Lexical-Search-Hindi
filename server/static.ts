95f05899bc7d9aff745ae4d9265c129ff83ecf2a
Gitsome189878 <130994081+Gitsome189878@users.noreply.github.com>
2026-02-13 16:02:29 +0530

    Update static.ts

--- server/static.ts
+++ server/static.ts
@@ -3,11 +3,9 @@ import path from "path";
 
 export function serveStatic(app: Express) {
   const clientDist = path.resolve(process.cwd(), "dist", "public");
-
   app.use(express.static(clientDist));
-
-  // SPA fallback
   app.get("*", (_req, res) => {
     res.sendFile(path.join(clientDist, "index.html"));
   });
 }
+
